13e6d205f6e605c1991ec031b39fe9e4ef5708e6
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-08-22 17:11:12 +0300

    Update index.js

--- keys/index.js
+++ keys/index.js
@@ -3,5 +3,5 @@ module.exports = {
     SESSION_SECRET: 'some secret value',
     SENDGRIP_API_KEY: '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',
     EMAIL_FROM: 'nodeshopkatya@gmail.com',
-    BASE_URL: 'http://localhost:6565'
+    BASE_URL: 'https://your-personal-tasks.herokuapp.com/'
 };
