71b16ca416b41c5db78bb974b2d9ace7e6a6efeb
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-08-23 00:14:27 +0300

    Update validators.js

--- utils/validators.js
+++ utils/validators.js
@@ -1,13 +1,24 @@
 const {body} = require('express-validator/check') 
 
 exports.registerValidators = [
-  body('email').isEmail().withMessage('Enter correct email'),
-  body('password', 'The password must to be at least 6 simbols').isLength({min: 6, max: 56}).isAlphanumeric(),
-  body('repeat').custom((value, {req}) => {
+  //EMAIL
+  body('email')
+  .isEmail().withMessage('Enter correct email')
+  .normalizeEmail(),
+  //PASSWORD
+  body('password', 'The password must to be at least 6 simbols')
+  .isLength({min: 6, max: 56}).isAlphanumeric()
+  .trim(),
+  //REPEAT
+  body('repeat')
+  .custom((value, {req}) => {
     if (value !== req.body.password) {
       throw new Error('Passwords not confirm')
     }
     return true
-  }),
-  body('name').isLength({min: 3}).withMessage('The name must to be min 2 simbols')
+  })
+  .trim(),
+  //NAME
+  body('name')
+  .isLength({min: 3}).withMessage('The name must to be min 2 simbols')
 ]
