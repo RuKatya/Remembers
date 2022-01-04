222407d1e79a21a253192d76868009b845084ae0
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-01-04 02:01:45 +0200

    remembers

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+//DATE
 const toDate = date => {
     return new Intl.DateTimeFormat('en-EN', {
         day: '2-digit',
@@ -13,10 +14,24 @@ document.querySelectorAll('.date').forEach(node => {
     node.textContent = toDate(node.textContent)
 })
 
+//MENU BUTTON
+const menuBtn = document.getElementById('menu');
+const sideMenu = document.getElementById('sideMenu');
+console.log(menuBtn)
+console.log(sideMenu)
+
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.classList.toggle("isOpen")
+})
+
+//REMOVE TASK AJAX
 const $remembers = document.querySelector('#remembers')
 
 if ($remembers) {
     $remembers.addEventListener('click', event => {
+        console.log('da')
+        console.log(event)
         if (event.target.classList.contains('deleteTask')) {
             const id = event.target.dataset.id
             const csrf = event.target.dataset.csrf
@@ -31,29 +46,33 @@ if ($remembers) {
                     if (tasks.length) {
                         const html = tasks.map(task => {
                             return `
-                            <div class="eachTask">
-                                        <div class="eachTask__text">
+                            <div class="remembers__eachTask">
+                                        <div class="remembers__eachTask--text">
                                             ${task.text}
                                         </div>
-                                        <div class="date eachTask__date">
+                                        <div class="date remembers__eachTask--date">
                                             ${toDate(task.date)}
                                         </div>
                                         <!-- EDIT BTN -->
                                         <div>
                                             <a href="/remembers/${task._id}/edit?allow=true"
-                                                class="eachTask__editBtn">Edit</a>
+                                                class="remembers__eachTask--editBtn">
+                                                <img src="./icons/editTask.png" alt="Edit">
+                                            </a>
                                         </div>
                                         <!-- DELETE BTN -->
                                         <div>
-                                            <button type="button" class="deleteTask eachTask__deleteBtn"
-                                                data-id="${task._id}" data-csrf="${csrf}">Delete</button>
+                                            <button type="button" class="deleteTask remembers__eachTask--deleteBtn">
+                                                <img class="deleteTask" src="./icons/delete.png" alt="Delete"
+                                                    data-id="${task._id}" data-csrf="${csrf}">
+                                            </button>
                                         </div>
                                     </div>
                                     `
                         }).join('')
-                        $remembers.querySelector('.remembers__tasks').innerHTML = html
+                        $remembers.querySelector('.remembers').innerHTML = html
                     } else {
-                        $remembers.querySelector('.remembers__tasks').innerHTML = `
+                        $remembers.querySelector('.remembers').innerHTML = `
                         <div class="remembers__noTask">
                             <div>There no tasks.</div>
                             <div>Just write your task and</div>
